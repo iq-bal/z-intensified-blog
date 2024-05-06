cec7b9a2988d59daca9891d523376943a5692a44
iqbal <iqbal.official.ac@gmail.com>
2024-05-06 07:03:21 +0600

    notification button added but non functional

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,11 +18,25 @@
     <header>
         <ul class="nav-links">
             <li><a href="/">Home</a></li>
-            <li class="center"><a href="/blogs/create">+Blog</a></li>
+            <li class="center"><a href="/blogs/create">+Blog</a></li>           
             @auth
+                <li class="center">
+                    <a href="/notifications">
+                        <i class="fa fa-bell fa-lg rounded-circle"></i>
+                    </a>
+                </li> 
+
+
+                {{-- <li class="upward">
+                    <a href="/users/{{ auth()->id() }}">
+                        <img src="{{ auth()->user()->dp_image ? asset('storage/' . auth()->user()->dp_image) : 'https://source.unsplash.com/600x400/?computer' }}" alt="Profile Picture" class="profile-picture" style="border-radius: 50%; width: 40px; height: 40px;">
+                    </a>
+                </li> --}}
+                
+                
                 {{-- <li class="upward"><a href="/blogs/manage">Manage</a></li> --}}
                 <li class="upward"><a href="/users/{{auth()->id()}}">Profile</a></li>
-                
+
                 <li class="forward">
                     <form action="/logout" method="POST" class="inline" style="display: inline-block; margin-right: 10px;">
                         @csrf
